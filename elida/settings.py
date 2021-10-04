2a3ef99831f156bd836586c78bdf378de49cecae
Martin Hanicinec <hanicinecm@gmail.com>
2021-10-04 15:09:37 +0200

    rename the apps

--- elida/settings.py
+++ elida/settings.py
@@ -28,8 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'species',
-    'lifetime',
+    'states',
+    'lifetimes',
     'api',
 ]
 
--- lifetimes/apps.py
+++ lifetimes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LifetimeConfig(AppConfig):
+class LifetimesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lifetime'
+    name = 'lifetimes'
