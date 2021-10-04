a18599eece1721f0a22e608514635c73edcb9b9c
Martin Hanicinec <hanicinecm@gmail.com>
2021-10-04 15:02:12 +0200

    add local_settings.py support, migrate to mysql

--- elida/settings.py
+++ elida/settings.py
@@ -11,19 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY, DEBUG, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6k#b3ey4!_p_)g62v+@d^#4de+8_^3_ofq-z59+p28ks9&l77q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'species',
+    'lifetime',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -68,17 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'elida.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
