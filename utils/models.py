16fcf33b776e7369c92c0146d380f42d00958c4a
Martin Hanicinec <hanicinecm@gmail.com>
2021-10-05 13:56:40 +0200

    add docstrings to states app

--- utils/models.py
+++ utils/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class ProvenanceMixin:
     time_added = models.DateTimeField(auto_now_add=True)
     time_modified = models.DateTimeField(auto_now=True)
+
+
+class ReprMixin:
+    def __repr__(self):
+        # noinspection PyUnresolvedReferences
+        return f'{self.id}:{self.__name__}({self})'
