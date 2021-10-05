bd98492d5ffcf1a4d3816d19e6d347ce502fbff4
Martin Hanicinec <hanicinecm@gmail.com>
2021-10-05 16:07:39 +0200

    restructure the apps

--- elida/settings.py
+++ elida/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'states',
+
+    'utils',
     'lifetimes',
     'api',
 ]
