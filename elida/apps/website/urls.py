ebe83dc1836697199b5fb5e2b55dd87ff38d8361
Martin Hanicinec <hanicinecm@gmail.com>
2021-10-16 00:05:47 +0200

    more implementation

--- elida/apps/website/urls.py
+++ elida/apps/website/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about, name='website-home'),
+    path('', views.home, name='website-home'),
     path('about/', views.about, name='website-about'),
+    path('data/', views.data, name='website-data'),
+    path('contact/', views.contact, name='website-contact'),
 ]
--- elida/apps/website/views.py
+++ elida/apps/website/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
-def about(request):
+def home(request):
     return render(request, 'about.html')
+
+
+def data(request):
+    return render(request, 'data.html', context={'title': 'Data'})
+
+
+def about(request):
+    return render(request, 'about.html', context={'title': 'About'})
+
+
+def contact(request):
+    return render(request, 'contact.html', context={'title': 'Contact'})
