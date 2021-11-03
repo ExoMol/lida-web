1cabbc971cb6f92da56827c6398c9f58730d5dfc
Martin Hanicinec <hanicinecm@gmail.com>
2021-11-03 09:01:47 +0100

    make the first go at the species server side datatable live

--- elida/apps/state/urls.py
+++ elida/apps/state/urls.py
@@ -4,5 +4,4 @@ from .views import StateListView
 
 urlpatterns = [
     path('list/<str:mol_slug>/', StateListView.as_view(), name='state-list'),
-    path('list-new/<str:mol_slug>/', StateListView.as_view(template_name='state_list_new.html'), name='state-list-new')
 ]
